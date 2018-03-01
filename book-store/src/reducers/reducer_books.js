f75b9216719f0cb1c46fe99c74d468ea07cbb283
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-03-02 00:22:42 +0100

    Better book data

--- book-store/src/reducers/reducer_books.js
+++ book-store/src/reducers/reducer_books.js
@@ -1,27 +1,27 @@
 const initialState = [
   {
     id: "1",
-    title: "Book 1"
+    title: "JavaScript The Good Parts"
   },
   {
     id: "2",
-    title: "Book 2"
+    title: "Harry Potter"
   },
   {
     id: "3",
-    title: "Book 3"
+    title: "Maze Runner"
   },
   {
     id: "4",
-    title: "Book 4"
+    title: "Hunger Games"
   },
   {
     id: "5",
-    title: "Book 5"
+    title: "War and Peace"
   }
 ];
 
-const books = (state = initialState, actions) => {
+const books = (state = initialState, action) => {
   return state;
 };
 
