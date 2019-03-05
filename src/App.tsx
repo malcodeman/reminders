f39e1136ccca1af4c7877768d7245799259a43cf
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-03-05 19:26:08 +0100

    Adds remove todo method

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
 import style from "./app.module.css";
-import { ReactComponent as PlusIcon } from "./plus.svg";
+import { ReactComponent as PlusSquareIcon } from "./plus-square.svg";
 import { ReactComponent as SquareIcon } from "./square.svg";
 import { ReactComponent as CheckSquareIcon } from "./check-square.svg";
+import { ReactComponent as XSquareIcon } from "./x-square.svg";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -45,6 +46,12 @@ function App() {
     setTodos(newTodos);
   }
 
+  function removeTodo(id: number): any {
+    const newTodos = todos.filter(todo => todo.id !== id);
+
+    setTodos(newTodos);
+  }
+
   return (
     <div className={style.app}>
       <main className={style.main}>
@@ -52,12 +59,12 @@ function App() {
           <input
             className={style.input}
             type="text"
-            placeholder="Add item..."
+            placeholder="✍️ Add item..."
             value={value}
             onChange={handleOnChange}
           />
-          <button className={style.plusBtn} onClick={handleSubmit}>
-            <PlusIcon className={style.plusIcon} />
+          <button className={style.plusSquareBtn} onClick={handleSubmit}>
+            <PlusSquareIcon className={style.plusSquareIcon} />
           </button>
         </form>
         <ul className={style.list}>
@@ -68,18 +75,39 @@ function App() {
                   todo.isCompleted ? style.listItemCompleted : style.listItem
                 }
                 key={todo.id}
-                onClick={() => toggleIsCompleted(todo.id)}
               >
                 {todo.isCompleted ? (
-                  <button className={style.checkSquareBtn}>
+                  <button
+                    className={style.checkSquareBtn}
+                    onClick={() => toggleIsCompleted(todo.id)}
+                  >
                     <CheckSquareIcon className={style.checkSquareIcon} />
                   </button>
                 ) : (
-                  <button className={style.squareBtn}>
+                  <button
+                    className={style.squareBtn}
+                    onClick={() => toggleIsCompleted(todo.id)}
+                  >
                     <SquareIcon className={style.squareIcon} />
                   </button>
                 )}
                 <span>{todo.value}</span>
+                <button
+                  className={
+                    todo.isCompleted
+                      ? style.xSquareBtnCompleted
+                      : style.xSquareBtn
+                  }
+                  onClick={() => removeTodo(todo.id)}
+                >
+                  <XSquareIcon
+                    className={
+                      todo.isCompleted
+                        ? style.xSquareIconCompleted
+                        : style.xSquareIcon
+                    }
+                  />
+                </button>
               </li>
             );
           })}
